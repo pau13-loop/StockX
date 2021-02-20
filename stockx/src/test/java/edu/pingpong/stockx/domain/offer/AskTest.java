453d9fa69ddb4fdde1a8b923ecafbae22907be4b
pau <pllinas@cifpfbmoll.eu>
2021-02-20 01:15:00 +0100

    test: :white_check_mark: Adding Ask class tests

---
+++ stockx/src/test/java/edu/pingpong/stockx/domain/offer/AskTest.java
@@ -0,0 +1,29 @@
+package edu.pingpong.stockx.domain.offer;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AskTest {
+    
+    private Ask ask = null;
+
+    @Before
+    public void setupBid() {
+
+        ask = new Ask("14", 400);
+    }
+
+    @Test
+    public void sizaAndValueTest() {
+
+        assertEquals("14", ask.size());
+        assertEquals(400, ask.value());
+    }
+
+    @Test
+    public void toStringTest() {
+
+        assertEquals("14\t400\n", ask.toString());
+    }
+}
