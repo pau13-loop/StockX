f46853c937ec9f94c97383daf22ab05af57c6f7c
pau <pllinas@cifpfbmoll.eu>
2021-02-20 01:13:31 +0100

    docs: :memo: New user story

---
+++ stockx/src/main/java/edu/pingpong/stockx/appStockx/Stockx.java
@@ -0,0 +1,57 @@
+package edu.pingpong.stockx.appStockx;
+
+import edu.pingpong.stockx.domain.item.*;
+import edu.pingpong.stockx.domain.offer.*;
+
+import java.util.List;
+
+import edu.pingpong.stockx.domain.criteria.*;
+
+
+public class Stockx
+{
+    public static void main( String[] args )
+    {
+         /**
+         * Crear la zapatilla 
+         * y printar sus datos.
+         * 
+         * Lee la funcion draw() del main
+         * para conocer que propiedades
+         * de la zapatilla han de printarse.
+         */
+
+        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        System.out.println(Stockx.draw(sneaker));
+
+        /**
+         * Crear bids
+         * y añadirlas a la zapatilla
+         * en sus offers.
+         */
+
+        Bid bid = new Bid("13", 550);
+        sneaker.add(bid);
+        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Bid("9.5", 479));
+        sneaker.add(new Bid("13", 338));
+        sneaker.add(new Bid("9.5", 480));
+    }
+    
+
+    public static String draw(Item sneaker) {
+        return
+        "\n\n\t\t" + sneaker.getAsk() + " Buy\t" 
+        + sneaker.getBid() + " Sell \n" +  
+
+        "\t\t" + " _    _" + "\n" +
+        "\t\t" + "(_\\__/(,_" + "\n" +
+        "\t\t" + "| \\ `_////-._" + "\n" +
+        "\t\t" + "J_/___\"=> __/`\\" + "\n" +
+        "\t\t" + "|=====;__/___./" + "\n" +
+        "\t\t" + "\'-\'-\'-\"\"\"\"\"\"\"`" + "\n" +
+        
+        "\t" + sneaker.toString() + "\n" +
+        "\t\tlast sale: " + sneaker.getSale();
+    }
+}
