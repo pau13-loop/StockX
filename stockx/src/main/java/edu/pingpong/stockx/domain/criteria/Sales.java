67239097b5a58e3cbb4d9040c567741e1e07bb34
pau <pllinas@cifpfbmoll.eu>
2021-02-20 02:00:47 +0100

    feat: :sparkles: Implementation sales class

---
+++ stockx/src/main/java/edu/pingpong/stockx/domain/criteria/Sales.java
@@ -0,0 +1,24 @@
+package edu.pingpong.stockx.domain.criteria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pingpong.stockx.domain.item.*;
+import edu.pingpong.stockx.domain.offer.*;
+
+public class Sales implements Criteria{
+
+    List<Offer> sales = new ArrayList<Offer>();
+
+    public Sales() {}
+
+    @Override
+    public List<Offer> checkCriteria(Item sneaker){
+        for(Offer sneak : sneaker.offers()){
+        if(sneak instanceof Ask){
+            sales.add(sneak);
+        }
+    }
+    return sales;
+    }
+}
