a2573910b1ceb166515a4f76e6b9ea29ef184656
pau <pllinas@cifpfbmoll.eu>
2021-02-25 20:29:14 +0100

    test: :white_check_mark: Creation test cases for MaxTest class

---
+++ stockx/src/test/java/edu/pingpong/stockx/domain/criteria/MaxTest.java
@@ -0,0 +1,47 @@
+package edu.pingpong.stockx.domain.criteria;
+
+import org.junit.Test;
+
+import edu.pingpong.stockx.domain.item.Item;
+import edu.pingpong.stockx.domain.item.Sneaker;
+import edu.pingpong.stockx.domain.offer.Bid;
+
+public class MaxTest {
+    
+    Item sneaker = null;
+    Criteria bids = null;
+    Criteria maxBid = null;
+
+    @Test
+    public void checkMaxBidTest1() {
+        sneaker = new Sneaker("Nike", "Performance");
+        bids = new Bids();
+        maxBid = new MaxBid();
+
+        sneaker.add(new Bid("45", 300));
+        sneaker.add(new Bid("80", 500));
+        sneaker.add(new Bid("12", 78));
+        sneaker.add(new Bid("74", 200));
+        sneaker.add(new Bid("1", 15));
+
+        assertArrayEquals(500, maxBid.checkCriteria(sneaker).get(0).value());
+    }
+
+    private void assertArrayEquals(int i, int value) {
+    }
+
+    @Test
+    public void checkMaxBidTest2() {
+        sneaker = new Sneaker("Nike", "Performance");
+        bids = new Bids();
+        maxBid = new MaxBid();
+
+        sneaker.add(new Bid("45", 100));
+        sneaker.add(new Bid("80", 150));
+        sneaker.add(new Bid("12", 78));
+        sneaker.add(new Bid("74", 200));
+        sneaker.add(new Bid("1", 15));
+
+        assertArrayEquals(200, maxBid.checkCriteria(sneaker).get(0).value());
+    }
+}
