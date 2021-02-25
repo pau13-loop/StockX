ec63002b8b18b0874fee21d31443ceb18da3b278
pau <pllinas@cifpfbmoll.eu>
2021-02-25 20:27:55 +0100

    feat: :sparkles: Implementation MaxBid class

--- stockx/src/main/java/edu/pingpong/stockx/domain/criteria/MaxBid.java
+++ stockx/src/main/java/edu/pingpong/stockx/domain/criteria/MaxBid.java
@@ -1,7 +1,9 @@
 package edu.pingpong.stockx.domain.criteria;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import edu.pingpong.stockx.domain.item.*;
 import edu.pingpong.stockx.domain.offer.*;
@@ -10,20 +12,11 @@ public class MaxBid implements Criteria{
 
     List<Offer> maxBid = new ArrayList<Offer>();
 
-    public MaxBid() {}
-
     @Override
     public List<Offer> checkCriteria(Item sneaker) {
-
-        Offer bid = new Bid("46", 0);
-
-        for(Offer sneak : sneaker.offers()) {
-            if(sneak instanceof Bid && sneak.value() > bid.value()) {
-                    bid = sneak;
-            }
-        }
-    maxBid.add(bid);
-    return maxBid;
-    }
     
+    Comparator<Offer> comparator = Comparator.comparing( Offer::value);
+
+    return sneaker.offers().stream().filter(b -> b instanceof Bid).max(comparator).stream().collect(Collectors.toList());
+    }
 }
