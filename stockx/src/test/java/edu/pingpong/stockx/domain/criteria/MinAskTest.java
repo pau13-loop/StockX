c73a563144fd50b6488520b4dac93df9b1d50a3c
pau <pllinas@cifpfbmoll.eu>
2021-03-06 18:53:15 +0100

    test: :white_check_mark: Added some more new test cases

--- stockx/src/test/java/edu/pingpong/stockx/domain/criteria/MinAskTest.java
+++ stockx/src/test/java/edu/pingpong/stockx/domain/criteria/MinAskTest.java
@@ -1,47 +1,49 @@
 package edu.pingpong.stockx.domain.criteria;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
 
 import edu.pingpong.stockx.domain.item.Item;
 import edu.pingpong.stockx.domain.item.Sneaker;
 import edu.pingpong.stockx.domain.offer.Ask;
+import edu.pingpong.stockx.domain.offer.Offer;
+import edu.pingpong.stockx.domain.offer.Sale;
 
 public class MinAskTest {
     
     Item sneaker = null;
-    Criteria bids = null;
-    Criteria maxAsk = null;
+    Criteria asks = null;
+    Criteria minAsk = null;
 
     @Test
     public void checkMinAskTest1() {
         sneaker = new Sneaker("Nike", "Performance");
-        bids = new Asks();
-        maxAsk = new MinAsk();
+        asks = new Asks();
+        minAsk = new MinAsk();
 
-        sneaker.add(new Ask("45", 300));
-        sneaker.add(new Ask("80", 500));
+        sneaker.add(new Ask("12", 300));
+        sneaker.add(new Ask("8", 500));
         sneaker.add(new Ask("12", 78));
-        sneaker.add(new Ask("74", 200));
-        sneaker.add(new Ask("1", 15));
+        sneaker.add(new Ask("9", 200));
+        sneaker.add(new Ask("7", 15));
+        sneaker.add(new Sale("10", 100));
+        sneaker.add(new Sale("9", 500));
+        sneaker.add(new Sale("8", 200));
 
-        assertArrayEquals(15, maxAsk.checkCriteria(sneaker).get(0).value());
-    }
-
-    private void assertArrayEquals(int i, int value) {
+        assertEquals(15, minAsk.checkCriteria(sneaker).get(0).value());
     }
 
     @Test
-    public void checkMinAskTest2() {
+    public void noMinAskTest() {
         sneaker = new Sneaker("Nike", "Performance");
-        bids = new Asks();
-        maxAsk = new MinAsk();
+        asks = new Asks();
+        minAsk = new MinAsk();
+        List<Offer> minA = minAsk.checkCriteria(sneaker);
+        sneaker.setAsk(minA.isEmpty()? 0 : minA.get(0).value());
 
-        sneaker.add(new Ask("45", 100));
-        sneaker.add(new Ask("80", 150));
-        sneaker.add(new Ask("12", 78));
-        sneaker.add(new Ask("74", 200));
-        sneaker.add(new Ask("1", 15));
-
-        assertArrayEquals(15, maxAsk.checkCriteria(sneaker).get(0).value());
+        assertEquals(0, sneaker.getAsk());
     }
 }
